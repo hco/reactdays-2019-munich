d8beb811cacb4e03b61488cedbb9e9748f20731b
Elmar Burke <hallo@elmarburke.de>
2019-03-19 15:56:58 +0100

    hooks

--- src/components/MessageCompose.tsx
+++ src/components/MessageCompose.tsx
@@ -1,13 +1,20 @@
-import React, { FormEvent } from 'react';
+import React, { FormEvent, useState } from 'react';
 
 const MessageCompose = () => {
+  const [inputValue, setInputValue] = useState('');
+
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    console.log('Hallo Message');
+    console.log(inputValue);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      <input type="text" value={inputValue} onChange={handleChange} />
       <button type="submit">Send 📬</button>
     </form>
   );
