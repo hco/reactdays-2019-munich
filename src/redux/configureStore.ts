03ac1478e97c268546659ff5f6140e18b92623e6
Elmar Burke <hallo@elmarburke.de>
2019-03-20 15:01:42 +0100

    elmar macht einen commit

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -1,8 +1,53 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { Message } from '../domain/Message';
 
-const initialState = {};
-const reducer = (state = initialState, action) => state;
+interface ApplicationState {
+  messages: Record<string, Message>;
+  /*
+  messages: {
+    [key: string]: Messages
+  }
+  */
+}
+
+const initialState: ApplicationState = {
+  messages: {
+    '2263EDA5-718F-4940-A2DF-E51CFD6D38C4': {
+      id: '2263EDA5-718F-4940-A2DF-E51CFD6D38C4',
+      message: 'Hallo Welt',
+      date: 1552294754374,
+      author: 'Elmarrr'
+    }
+  }
+};
+
+interface AddMessageAction {
+  type: 'MESSAGE/ADD';
+  payload: Message;
+}
+
+interface InitAction {
+  type: '@@INIT';
+}
+
+type Actions = AddMessageAction | InitAction;
+
+const reducer = (state = initialState, action: Actions): ApplicationState => {
+  switch (action.type) {
+    case 'MESSAGE/ADD':
+      return {
+        ...state,
+        messages: {
+          ...state.messages,
+          // Das ist eine Computed Property
+          [action.payload.id]: action.payload
+        }
+      };
+    default:
+      return state;
+  }
+};
 
 export const configureStore = () => {
   // Jetzt wird's komisch...
